427c61c8065766b88813c6410d5a16cae3824c0a
Zenslim <zenslim2010@gmail.com>
2025-12-09 16:24:49 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Suspense } from 'react'
