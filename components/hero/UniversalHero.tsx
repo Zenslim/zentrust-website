a156a5beb92ab66c8a57ddf40ca1da3ac34376d9
Zenslim <zenslim2010@gmail.com>
2025-12-20 21:38:42 +0545

    Update UniversalHero.tsx

--- components/hero/UniversalHero.tsx
+++ components/hero/UniversalHero.tsx
@@ -1,8 +1,21 @@
 "use client";
 
 import { HeroShell } from "./HeroShell";
+import { RitualPause } from "./RitualPause";
 import { defaultHero, type HeroDefinition } from "./createHero";
 
 export function UniversalHero({ hero }: { hero?: HeroDefinition }) {
-  return <HeroShell hero={hero ?? defaultHero} />;
+  const resolvedHero = hero ?? defaultHero;
+
+  return (
+    <>
+      {/* Text-only hero */}
+      <HeroShell hero={resolvedHero} />
+
+      {/* Ritual trigger + fullscreen overlay (layout-free) */}
+      {resolvedHero.ritual && (
+        <RitualPause {...resolvedHero.ritual} />
+      )}
+    </>
+  );
 }
