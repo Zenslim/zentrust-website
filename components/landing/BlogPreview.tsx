19ac7761c6c8947abffcd73001ef7c1ccd7f5f23
Zenslim <zenslim2010@gmail.com>
2025-12-26 13:57:54 +0545

    Delete components/landing directory

--- components/landing/BlogPreview.tsx
+++
@@ -1,7 +0,0 @@
-import { getLatestBlogPosts } from '@/lib/blog'
-import { BlogPreviewClient } from './BlogPreviewClient'
-
-export async function BlogPreview() {
-  const posts = getLatestBlogPosts(3)
-  return <BlogPreviewClient posts={posts} />
-}
